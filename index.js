c7edb0339dbab6710a7bbe79064b257f99f9b5c4
GabrielPassarinVicente <112556559+GabrielPassarinVicente@users.noreply.github.com>
2024-04-24 14:26:24 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+// Exemplo de JavaScript simples
+document.addEventListener('DOMContentLoaded', function() {
+    // Adicione aqui qualquer script JavaScript adicional que você precise
+    console.log('O script JavaScript está funcionando!');
+});
